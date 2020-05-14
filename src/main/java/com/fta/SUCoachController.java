bfa0954f737c794da7e3db67494de23123bbb3af
sorin.pui <puisorin99@gmail.com>
2020-05-14 22:23:36 +0300

    changes for the validation controllers

--- src/main/java/com/fta/SUCoachController.java
+++ src/main/java/com/fta/SUCoachController.java
@@ -1,8 +1,5 @@
 package com.fta;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -11,8 +8,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import com.google.gson.Gson;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import org.json.simple.JSONObject;
@@ -23,46 +19,40 @@ import org.json.simple.parser.ParseException;
 import java.io.*;
 import java.util.*;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class SUCoachController {
 
         public JSONArray jrr = new JSONArray();
 
         @FXML
-        private Button register;
-
-        @FXML
         private TextField username, password, fullname, adresa, numartelefon, varsta;
 
         @FXML
-        @SuppressWarnings("unchecked")
         private void submitData() throws IOException, ParseException {
 
                 JSONObject jo = new JSONObject();
                 JSONParser jp = new JSONParser();
 
-                try{
-                        FileReader file = new FileReader("usersCoach.json");
-                        jrr=(JSONArray) jp.parse(file);
-                }catch(Exception e){
+                try {
+                        FileReader file = new FileReader("users.json");
+                        jrr = (JSONArray) jp.parse(file);
+                } catch(Exception e) {
                         e.printStackTrace();
                 }
-                jo.put("Type","Coach");
-                jo.put("User",username.getText());
-                jo.put("Pass",password.getText());
-                jo.put("Full Name",fullname.getText());
-                jo.put("Adresa",adresa.getText());
-                jo.put("Number",numartelefon.getText());
-                jo.put("Age",varsta.getText());
+
+                jo.put("Type", "Coach");
+                jo.put("User", username.getText());
+                jo.put("Pass", password.getText());
+                jo.put("Full Name", fullname.getText());
+                jo.put("Adresa", adresa.getText());
+                jo.put("Number", numartelefon.getText());
+                jo.put("Age", varsta.getText());
                 jrr.add(jo);
 
-                if(checkAvailability(jo)==1){
+                if(checkAvailability(jo)==1) {
                         System.out.println("Already registered");
                 }else {
                         try {
-                                FileWriter file = new FileWriter("usersCoach.json");
+                                FileWriter file = new FileWriter("users.json");
                                 file.write(jrr.toJSONString());
                                 file.close();
                         } catch (Exception e) {
@@ -72,11 +62,18 @@ public class SUCoachController {
         }
 
         @FXML
-        private int checkAvailability(JSONObject jsonObject){
+        private int checkAvailability(JSONObject jsonObject) {
+
                 int size = jrr.size();
 
-                for(int i = 0; i<size-1;i++) {
-                        if (jsonObject.equals(jrr.get(i))) {
+                JSONObject temp;
+
+                for(int i = 0; i <size - 1; i++) {
+
+                        temp = (JSONObject) jrr.get(i);
+
+                        if (temp.get("User").equals(jsonObject.get("User"))) {
+
                                 Alert a = new Alert(Alert.AlertType.WARNING);
                                 a.setContentText("Already registered. Press Back and Sign In");
                                 a.show();
